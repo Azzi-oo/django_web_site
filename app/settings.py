759557e2788b94b686b8b8fedc5b2f96750de6ce
Azzi-oo <yanberdin.az@yandex.ru>
2024-01-29 10:52:01 +0500

    add new app carts

--- app/settings.py
+++ app/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'main',
     'goods',
     'users',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -113,3 +114,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/user/login/'
